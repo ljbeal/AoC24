469fdc9ecafd2a8055c0b9bab30ef2adfdfea1f7
Louis Beal <louis.j.beal@gmail.com>
2024-12-03 10:16:33 +0100

    Add some comments

--- Day03/solution.py
+++ Day03/solution.py
@@ -11,11 +11,19 @@ class Solver:
         return self._data
 
     def run(self, flow: bool) -> int:
+        """
+        Run the day 3 solution
+
+        Args:
+            flow:
+                enable the flow control of part 2
+        """
         # first, get each pair and extract the integers and their location index
         instruct_mul = {}
         for mul in re.finditer(r"mul\((\d*),(\d*)\)", self.data):
             instruct_mul[mul.start(0)] = (mul.group(1), mul.group(2))
-
+        # generate a list of "breakpoints" where multiplication should be turned on or off
+        # but only if flow control is enabled
         do = []
         dont = []
         if flow:
@@ -24,18 +32,18 @@ class Solver:
 
             for m in re.finditer(r"don't\(\)", self.data):
                 dont.append(m.start(0))
-
+        # keep a running total, summing all valid mul(a,b) strings
         sum = 0
-        enable = True
+        enable = True  # start with addition enabled
         for idx, pair in instruct_mul.items():
-
+            # consume do and dont lists, enabling or disabling as wed go
             if len(do) > 0 and idx > do[0]:
                 enable = True
                 del do[0]
             if len(dont) > 0 and idx > dont[0]:
                 enable = False
                 del dont[0]
-
+            # if we can, sum
             if enable:
                 sum += int(pair[0]) * int(pair[1])
 
