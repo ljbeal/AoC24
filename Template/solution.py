c4b1dac8c02c2b7c6e0acd42776064ead96ce0fa
ljbeal <louis.j.beal@gmail.com>
2024-12-07 11:42:23 +0100

    Update stubs

--- Template/solution.py
+++ Template/solution.py
@@ -6,7 +6,14 @@ class Solver(BaseSolver):
     def __init__(self, inp: str):
         super().__init__(inp=inp)
 
+    def run(self):
+        return NotImplemented
+
 
 if __name__ == "__main__":
 
     test_1 = Solver(inp="Input/input_test.txt")
+
+    test_1_run = test_1.run()
+
+    assert test_1_run == 0, test_1_run
