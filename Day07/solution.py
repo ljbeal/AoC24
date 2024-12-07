086c2ce95350ab2094c10d3101347f6b91ddcb60
ljbeal <louis.j.beal@gmail.com>
2024-12-07 12:44:50 +0100

    Part 2 solved

--- Day07/solution.py
+++ Day07/solution.py
@@ -10,7 +10,7 @@ class Solver(BaseSolver):
     def __init__(self, inp: str):
         super().__init__(inp=inp)
 
-    def run(self):
+    def run(self, concat: bool = False):
 
         achievable = []
         for row in self.rows:
@@ -19,7 +19,7 @@ class Solver(BaseSolver):
             target = int(target)
             input = [int(item.strip()) for item in input.split(" ") if item.strip() != ""]
 
-            print(f"Assessing inputs: {input} for target {target}")
+            # print(f"Assessing inputs: {input} for target {target}")
 
             # max_reachable = math.prod(input)
             # the "largest" operation we can do is multiplication
@@ -35,10 +35,14 @@ class Solver(BaseSolver):
             # That trailing +1 trips up the prod method
 
             # now "brute force" with permutations
-            operations = itertools.product(["+", "*"], repeat=len(input)-1)
+            available_ops = ["+", "*"]
+            if concat:
+                available_ops.append("||")
+
+            operations = itertools.product(available_ops, repeat=len(input)-1)
 
             for opset in operations:
-                print(f"\tperforming: {opset}. Result:", end=" ")
+                # print(f"\tperforming: {opset}. Result:", end=" ")
 
                 tmp = input[0]
                 for idx in range(len(input) - 1):
@@ -47,28 +51,35 @@ class Solver(BaseSolver):
 
                     if op == "+":
                         tmp += v
-                    else:
+                    elif op == "*":
                         tmp *= v
+                    elif op == "||":
+                        tmp = int(f"{tmp}{v}")
 
-                print(tmp)
+                # print(tmp)
                 if tmp == target:
-                    print(f"\tTarget Achieved, storing {target}")
+                    # print(f"\tTarget Achieved, storing {target}")
                     achievable.append(target)
                     break
 
         return sum(achievable)
 
 
-
 if __name__ == "__main__":
 
-    test_1 = Solver(inp="Input/input_test.txt")
+    test = Solver(inp="Input/input_test.txt")
 
     t0 = time.perf_counter()
-    test_1_run = test_1.run()
-
+    test_1_run = test.run()
     print(f"Part 1 test, {time.perf_counter() - t0:.3f}s")
     assert test_1_run == 3749, test_1_run
+
+    t0 = time.perf_counter()
+    test_2_run = test.run(concat=True)
+    print(f"Part 1 test, {time.perf_counter() - t0:.3f}s")
+    assert test_2_run == 11387, test_2_run
+
+
 
     sol = Solver(inp="Input/input.txt")
 
@@ -77,3 +88,9 @@ if __name__ == "__main__":
 
     part_1 = sol.run()
     print(f"Part 1 result: {part_1} {time.perf_counter() - t0:.3f}s")
+
+    print("Running Part 2")
+    t0 = time.perf_counter()
+
+    part_2 = sol.run(concat=True)
+    print(f"Part 2 result: {part_2} {time.perf_counter() - t0:.3f}s")
