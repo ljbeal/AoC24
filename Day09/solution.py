3651058763e28bb4109f08b41f5f5acd37277bf8
Louis Beal <louis.j.beal@gmail.com>
2024-12-09 11:40:39 +0100

    Stubs for part 2

--- Day09/solution.py
+++ Day09/solution.py
@@ -1,6 +1,8 @@
 import copy
 import time
 
+from numpy.ma.core import compressed
+
 from lib.base_solver import BaseSolver
 
 
@@ -44,7 +46,7 @@ class Solver(BaseSolver):
         return self._space
 
     @property
-    def sequence(self):
+    def sequence(self) -> list:
         if self._sequence is not None:
             return self._sequence
 
@@ -62,7 +64,7 @@ class Solver(BaseSolver):
 
         return structure
 
-    def compress(self, sequence: str) -> str:
+    def compress(self, sequence: list) -> list:
         nspace = sequence.count(".")
         # print(f"there are {nspace} free space instances")
 
@@ -81,28 +83,43 @@ class Solver(BaseSolver):
 
         return tmp
 
-    def run(self) -> int:
+    def compress_nonfrag(self, sequence: list) -> list:
+
+        tmp = copy.deepcopy(self.sequence)
+        for item in sequence[::-1]:
+            nextspace = tmp.index(".")
+
+        return sequence
+
+
+    def run(self, frag: bool = True) -> int:
         print("Compressing... ", end="")
-        compressed = self.compress(self.sequence)
+        if frag:
+            compressed = self.compress(self.sequence)
+        else:
+            print()
+            compressed = self.compress_nonfrag(self.sequence)
 
         checksum = 0
         print("Done.\nGenerating checksum... ", end=" ")
         for idx, item in enumerate(compressed):
-            checksum += idx * item.id
+            if item != ".":
+                checksum += idx * item.id
 
         print("Done.")
 
         return checksum
 
 
-
 if __name__ == "__main__":
 
-    test_1 = Solver(inp="Input/input_test.txt")
+    test = Solver(inp="Input/input_test.txt")
 
-    test_1_run = test_1.run()
-
+    test_1_run = test.run()
     assert test_1_run == 1928, test_1_run
+
+    test_2_run = test.run(frag=False)
+    assert test_2_run == 2858, test_2_run
 
     sol = Solver(inp="Input/input.txt")
 
@@ -111,3 +128,9 @@ if __name__ == "__main__":
 
     part_1 = sol.run()
     print(f"Part 1 result: {part_1} {time.perf_counter() - t0:.3f}s")
+
+    print("Running Part 2")
+    t0 = time.perf_counter()
+
+    part_2 = sol.run(frag=False)
+    print(f"Part 1 result: {part_2} {time.perf_counter() - t0:.3f}s")
