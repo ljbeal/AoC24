f85e40ec4a38d81e725c2de3261d67811651acab
Louis Beal <louis.j.beal@gmail.com>
2024-12-12 18:54:00 +0100

    Update template

--- Template/solution.py
+++ Template/solution.py
@@ -15,15 +15,21 @@ class Solver(BaseSolver):
 if __name__ == "__main__":
 
     test_1 = Solver(inp="Input/input_test.txt")
-
     test_1_run = test_1.run()
-
     assert test_1_run == 0, test_1_run
 
-    sol = Solver(inp="Input/input.txt")
+    # test_2 = Solver(inp="Input/input_test.txt")
+    # test_2_run = test_2.run()
+    # assert test_2_run == 0, test_2_run
 
+    sol = Solver(inp="Input/input.txt")
     print("Running Part 1")
     t0 = time.perf_counter()
-
     part_1 = sol.run()
     print(f"Part 1 result: {part_1} {time.perf_counter() - t0:.3f}s")
+
+    # sol = Solver(inp="Input/input.txt")
+    # print("Running Part 2")
+    # t0 = time.perf_counter()
+    # part_2 = sol.run()
+    # print(f"Part 2 result: {part_2} {time.perf_counter() - t0:.3f}s")
